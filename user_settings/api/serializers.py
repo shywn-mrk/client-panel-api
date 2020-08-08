ce19e3cf58ea4db405b839bca129778d25a7be06
Shayan Karimi <shayankarimi0078@gmail.com>
2020-08-08 18:18:21 +0430

    User settings update method added

--- user_settings/api/serializers.py
+++ user_settings/api/serializers.py
@@ -2,18 +2,14 @@ from rest_framework import serializers
 from user_settings.models import Settings
 from accounts.api.serializers import UserSerializer
 
-class SettingsSerializer(serializers.HyperlinkedModelSerializer):
-    user = serializers.SerializerMethodField(read_only=True)
-
+class SettingsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Settings
         fields = (
+            'id',
             'user',
             'allow_registeration',
             'disable_balance_on_add',
             'disable_balance_on_edit'
         )
-
-    def get_user(self, setting):
-        serializer = UserSerializer(setting.user)
-        return serializer.data
+        depth = 1
--- user_settings/api/viewsets.py
+++ user_settings/api/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from user_settings.models import Settings
 from .serializers import SettingsSerializer
 
@@ -15,3 +16,27 @@ class SettingsViewSet(viewsets.ViewSet):
         serializer = SettingsSerializer(settings)
 
         return Response(serializer.data)
+
+
+    def update(self, request, pk=None):
+        # getting the user who is sending request
+        user_token = request.auth
+        user = Token.objects.get(key=user_token).user
+
+        # getting the sent data
+        data = {
+            "user": user,
+            "allow_registeration": request.data.get('allow_registeration'),
+            "disable_balance_on_add": request.data.get('disable_balance_on_add'),
+            "disable_balance_on_edit": request.data.get('disable_balance_on_edit')
+        }
+
+        serializer = SettingsSerializer(data=data)
+
+        if serializer.is_valid(raise_exception=True):
+            settings = Settings.objects.get(pk=pk)
+            settings.__dict__.update(**data)
+            settings.save()
+            return Response(serializer.data, status=HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
